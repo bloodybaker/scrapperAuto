6316ec92a76ed3a6cec41b4d7b7b25dedf7c51b8
Yevhenii Chykalov <gamchanel@gmail.com>
2020-05-16 10:15:36 +0300

    useless update

--- start.py
+++ start.py
@@ -21,10 +21,27 @@ def text(message):
     else:
         while(1):
             chrome_options = Options()
+            prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2, 'javascript': 2,
+                                                                'plugins': 2, 'popups': 2, 'geolocation': 2,
+                                                                'notifications': 2, 'auto_select_certificate': 2,
+                                                                'fullscreen': 2,
+                                                                'mouselock': 2, 'mixed_script': 2, 'media_stream': 2,
+                                                                'media_stream_mic': 2, 'media_stream_camera': 2,
+                                                                'protocol_handlers': 2,
+                                                                'ppapi_broker': 2, 'automatic_downloads': 2,
+                                                                'midi_sysex': 2,
+                                                                'push_messaging': 2, 'ssl_cert_decisions': 2,
+                                                                'metro_switch_to_desktop': 2,
+                                                                'protected_media_identifier': 2, 'app_banner': 2,
+                                                                'site_engagement': 2,
+                                                                'durable_storage': 2}}
+            chrome_options.add_experimental_option('prefs', prefs)
             chrome_options.add_argument('--headless')
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-dev-shm-usage')
-            driver = webdriver.Chrome("/home/steam_shop_ua/scrapperAuto/chromedriver",chrome_options=chrome_options)
+            chrome_options.add_argument("--disable-extensions")
+            driver = webdriver.Chrome("chromedriver",chrome_options=chrome_options)
+            driver.set_window_size(1920, 1080)
             driver.get("https://www.iaai.com/VehicleSearch/SearchDetails?Keyword=" + data[0] + "+" + data[1]);
             time.sleep(10)
             try:
