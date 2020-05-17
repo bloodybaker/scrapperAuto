8ce753e21acbd8e9730a53de72dcd1e011f612c4
Yevhenii Chykalov <gamchanel@gmail.com>
2020-05-17 20:52:20 +0300

    useless update

--- start.py
+++ start.py
@@ -20,31 +20,31 @@ def text(message):
         bot.reply_to(message, "Попробуй еще раз")
     else:
         while(1):
-            chrome_options = Options()
-            prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2, 'javascript': 2,
-                                                                'plugins': 2, 'popups': 2, 'geolocation': 2,
-                                                                'notifications': 2, 'auto_select_certificate': 2,
-                                                                'fullscreen': 2,
-                                                                'mouselock': 2, 'mixed_script': 2, 'media_stream': 2,
-                                                                'media_stream_mic': 2, 'media_stream_camera': 2,
-                                                                'protocol_handlers': 2,
-                                                                'ppapi_broker': 2, 'automatic_downloads': 2,
-                                                                'midi_sysex': 2,
-                                                                'push_messaging': 2, 'ssl_cert_decisions': 2,
-                                                                'metro_switch_to_desktop': 2,
-                                                                'protected_media_identifier': 2, 'app_banner': 2,
-                                                                'site_engagement': 2,
-                                                                'durable_storage': 2}}
-            chrome_options.add_experimental_option('prefs', prefs)
-            chrome_options.add_argument('--headless')
-            chrome_options.add_argument('--no-sandbox')
-            chrome_options.add_argument('--disable-dev-shm-usage')
-            chrome_options.add_argument("--disable-extensions")
-            driver = webdriver.Chrome("/home/steam_shop_ua/scrapperAuto/chromedriver",chrome_options=chrome_options)
-            driver.set_window_size(1920, 1080)
-            driver.get("https://www.iaai.com/VehicleSearch/SearchDetails?Keyword=" + data[0] + "+" + data[1]);
-            time.sleep(10)
             try:
+                chrome_options = Options()
+                prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2, 'javascript': 2,
+                                                                    'plugins': 2, 'popups': 2, 'geolocation': 2,
+                                                                    'notifications': 2, 'auto_select_certificate': 2,
+                                                                    'fullscreen': 2,
+                                                                    'mouselock': 2, 'mixed_script': 2, 'media_stream': 2,
+                                                                    'media_stream_mic': 2, 'media_stream_camera': 2,
+                                                                    'protocol_handlers': 2,
+                                                                    'ppapi_broker': 2, 'automatic_downloads': 2,
+                                                                    'midi_sysex': 2,
+                                                                    'push_messaging': 2, 'ssl_cert_decisions': 2,
+                                                                    'metro_switch_to_desktop': 2,
+                                                                    'protected_media_identifier': 2, 'app_banner': 2,
+                                                                    'site_engagement': 2,
+                                                                    'durable_storage': 2}}
+                chrome_options.add_experimental_option('prefs', prefs)
+                chrome_options.add_argument('--headless')
+                chrome_options.add_argument('--no-sandbox')
+                chrome_options.add_argument('--disable-dev-shm-usage')
+                chrome_options.add_argument("--disable-extensions")
+                driver = webdriver.Chrome("/home/steam_shop_ua/scrapperAuto/chromedriver",chrome_options=chrome_options)
+                driver.set_window_size(1920, 1080)
+                driver.get("https://www.iaai.com/VehicleSearch/SearchDetails?Keyword=" + data[0] + "+" + data[1]);
+                time.sleep(10)
                 filter_button = driver.find_element_by_xpath("/html/body/section/main/section[2]/div/div[1]/div[3]/a")
                 filter_button.click()
                 from_time = driver.find_element_by_xpath(
@@ -55,8 +55,7 @@ def text(message):
                 from_time.send_keys(data[2])
                 to_time.clear();
                 to_time.send_keys(data[3])
-                apply_button = driver.find_element_by_xpath(
-                    "/html/body/section/main/section[2]/div/div[1]/div[3]/div/form/div/button")
+                apply_button = driver.find_element_by_xpath("/html/body/section/main/section[2]/div/div[1]/div[3]/div/form/div/button")
                 apply_button.click()
                 time.sleep(10)
                 title_data = driver.find_element_by_xpath("/html/body/section/main/section[3]/section/div/div[2]/div/div[1]/div[3]/h3/a")
